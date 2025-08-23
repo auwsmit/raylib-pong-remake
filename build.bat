bdfcd5afda3c00a7a6a4dc44d4e884f8a947d5fd
Austin W Smith <auwsmit@gmail.com>
2025-08-23 03:58:17 -0700

    refactor build.bat script (use `build cmake` now)
    After some consideration, I decided I like the idea of just one build
    script which has minimal dependencies (just the C compiler + libraries).
    But it can still be a wrapper for CMake at the same time.
    Best of both worlds imo
    
    now I just gotta convert the makefile into batch script... yaaaaaay..

--- build.bat
+++ build.bat
@@ -1,67 +1,116 @@
 @echo off
 
-:: --- Usage Notes ------------------------------------------------------------
+:: NOTE: only CMake commands are working right now
+
+:: Usage Notes
+:: ----------------------------------------------------------------------------
 ::
-:: This build script is a wrapper for CMake to build this project on Windows.
+:: This build script can be used in two main ways:
+:: 1. `build` to simply compile the game with a C compiler
+:: 2. `build cmake` to setup and run the CMake build system
+:: Either way, you will find the output executable in the project directory.
 ::
-:: With no arguments, running `build` will download and build raylib if needed,
-:: generate a Visual Studio solution for the game, build it with MSVC, and then
-:: place a copy of the game executable in the repo directory. This script can
-:: take a single argument that specifies which compiler to use, or which
-:: platform to compile for.
+:: Additional arguments can be given to choose the compiler, compiler settings,
+:: or to clean up old build files.
 ::
-:: You can find the generated Visual Studio solution within build\desktop\
+:: Below is a non-exhaustive list of arguments you can use:
+:: `build release` -> optimized build, no debug symbols
+:: `build clang`   -> use clang compiler
+:: `build msvc`    -> use msvc compiler, and make .pdb debug files
+:: `build web`     -> compile to web assembly with emscripten
+:: `build clean`   -> delete old generated build files (excluding CMake)
+:: CMake mode:
+:: `build cmake`       -> setup and build using CMake
+:: `build cmake gcc`   -> use MinGW compiler instead of MSVC
+:: `build cmake web`   -> compile to web assembly with emscripten
+:: `build cmake clean` -> delete CMake's old build files
 ::
-:: Below is a list of arguments you can use:
-:: `build gcc`  --> use MinGW compiler instead of MSVC
-:: `build web`  --> compile to web assembly with emscripten
-:: `build clean` -> delete all previously generated build files
+:: Notes for CMake:
+:: - CMake will automatically download and build raylib if needed
+:: - You can find the generated Visual Studio solution in build\desktop\
 ::
 :: ----------------------------------------------------------------------------
 
-:: cd to the script directory
-cd "%~dp0"
+set script_dir=%~dp0
 
-:: Build for desktop
+:: Unpack Arguments
 :: ----------------------------------------------------------------------------
-if "%1"=="" (
-    cmake -DPLATFORM=Desktop -B build\desktop
-    cmake --build build\desktop
-    del /q game.exe
-    copy build\desktop\Debug\game.exe .
-    goto :eof
+for %%a in (%*) do set "%%a=1"
+if "%clean%"=="1" echo [clean mode] && goto :clean
+if not "%release%"=="1" set debug=1
+if "%debug%"=="1"   set release=0 && echo [debug mode]
+if "%release%"=="1" set debug=0   && echo [release mode]
+if "%web%"=="1" set clang=0 && set msvc=0 && set gcc=0
+if "%cmake%"=="1" ( echo [cmake mode]
+    if "%gcc%"=="1" echo [cmake-mingw mode]
+    if "%~2"==""    echo [msvc compile]
+) else (              echo [default mode]
+    if "%clang%"=="1" set msvc=0 && echo [clang compile]
+    if "%msvc%"=="1"  set clang=0 && echo [msvc compile]
+)
+if "%~1"==""      echo [gcc compile]
+if "%web%"=="1"   echo [web compile]
+
+:: Project Specific Settings
+:: ----------------------------------------------------------------------------
+set output=game
+set cmake_build_dir=%script_dir%\build
+
+:: CMake Definitions
+:: ----------------------------------------------------------------------------
+if "%cmake%"=="1" (
+    set cmake_setup_desktop=cmake -DOUTPUT_NAME=%output% -B "%cmake_build_dir%\desktop" -DPLATFORM=Desktop
+    set cmake_build_desktop=cmake --build "%cmake_build_dir%\desktop"
+    set cmake_setup_web=call emcmake cmake -DOUTPUT_NAME=%output% -DCMAKE_EXECUTABLE_SUFFIX=".html" -B "%cmake_build_dir%\web" -DPLATFORM=Web
+    set cmake_build_web=call emmake make -C "%cmake_build_dir%\web"
 )
 
-:: Build with gcc, useful if Visual Studio is unavailable
+:: Per-Build Settings
 :: ----------------------------------------------------------------------------
-if "%1"=="gcc" (
-    cmake -DPLATFORM=Desktop -G "MinGW Makefiles" -B build\desktop
-    cmake --build build\desktop
-    del /q game.exe
-    copy build\desktop\game.exe .
-    goto :eof
+if "%cmake%"=="1"   (
+    if "%web%"=="1" (
+        set output_dir=%cmake_build_dir%\web
+        set cmake_setup_cmd=%cmake_setup_web%
+        set cmake_build_cmd=%cmake_build_web%
+    ) else (
+        set output_dir=%cmake_build_dir%\desktop\Debug
+        set cmake_setup_cmd=%cmake_setup_desktop%
+        set cmake_build_cmd=%cmake_build_desktop%
+    )
+
+    if "%release%"=="1" if "%msvc%"=="1" set cmake_setup_flags=--config Release
+    if "%gcc%"=="1"     set cmake_setup_flags=-G "MinGW Makefiles"
+    if "%release%"=="1" set cmake_setup_flags=-DCMAKE_BUILD_TYPE=Release %cmake_setup_flags%
+    if "%debug%"=="1"   set cmake_setup_flags=-DCMAKE_BUILD_TYPE=Debug %cmake_setup_flags%
 )
 
-:: Build for web
+:: Build Target
 :: ----------------------------------------------------------------------------
-if "%1"=="web" (
-    mkdir build\web
-    pushd build\web
-    call emcmake cmake ..\.. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html"
-    call emmake make
-    popd
-    del /q game.html game.js game.wasm
-    copy build\web\game.html .
-    copy build\web\game.js .
-    copy build\web\game.wasm .
-    goto :eof
+if "%cmake%"=="1" (
+    %cmake_setup_cmd% %cmake_setup_flags%
+    %cmake_build_cmd%
+    if "%web%"=="1" (
+        del /q "%script_dir%\%output%.html"
+        del /q "%script_dir%\%output%.js"
+        del /q "%script_dir%\%output%.wasm"
+        copy "%output_dir%\%output%.html" "%script_dir%"
+        copy "%output_dir%\%output%.js" "%script_dir%"
+        copy "%output_dir%\%output%.wasm" "%script_dir%"
+    ) else (
+        :: desktop
+        del /q "%script_dir%\%output%.exe"
+        copy "%output_dir%\%output%.exe" "%script_dir%"
+    )
 )
 
-:: Clean up old files
+:: Clean Up
 :: ----------------------------------------------------------------------------
-if "%1"=="clean" (
-    rmdir /s /q build
-    del /q game.*
+:clean
+if not "%clean%"=="1" goto :eof
+if "%cmake%"=="1" (
+    rmdir /s /q "%script_dir%\build"
+    del /q "%script_dir%\game.*"
     echo CMake build files cleaned
-    goto :eof
+) else (
+    REM echo Build files cleaned
 )
